d22bde2203fd05983126ca7c572cac078f937090
Puria Nafisi Azizi <puria@dyne.org>
2024-12-03 03:49:43 +0100

    feat: add license headers and reuse check (#13)

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2024 The Forkbomb Company
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 NODE_MODULES = node_modules
 PACKAGE_JSON = package.json
 BUN_LOCK = bun.lockb
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2024 The Forkbomb Company
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 import { defineConfig } from "vite";
 import UnoCSS from "unocss/vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
